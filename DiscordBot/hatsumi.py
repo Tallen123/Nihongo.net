61dab13a744d1375aa315753c4a4ec90f21f0de2
Tallen123 <tallenthompson@icloud.com>
2021-10-25 23:13:26 +0100

    Added Custom Welcome message

--- DiscordBot/hatsumi.py
+++ DiscordBot/hatsumi.py
@@ -6,6 +6,11 @@ import requests
 from discord import channel
 from discord.embeds import Embed
 from dotenv import load_dotenv
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members=True
+client=discord.Client(intents=intents)
 
 url = "static\Alphabet.json".lower()
 
@@ -16,13 +21,11 @@ dict.update(kanjilist,Kanji['N2'])
 dict.update(kanjilist,Kanji['N3'])
 dict.update(kanjilist,Kanji['N4'])
 dict.update(kanjilist,Kanji['N5'])
-print(kanjilist)
+
 Configdata = open('DiscordBot\Config.json')
 token = json.load(Configdata)['BOT_TOKEN']
 
-client = discord.Client()
 PREFIX = "!"
-
 
 @client.event
 async def on_ready():
@@ -35,19 +38,19 @@ async def on_message(message):
     if message.author == client.user:#checks the message auther
         return
 
-    if message.content == PREFIX + 'Kanji':#kanji Command
+    if message.content == PREFIX + 'kanji':#kanji Command
         rand = random.choice(list(kanjilist))
         response = rand,kanjilist[rand]
         await message.channel.send(response)
 
-    if message.content == PREFIX + 'Help':#Help Command
+    if message.content == PREFIX + 'help':#Help Command
         embed = discord.Embed(
             title="Help",
             description="Commands help",
             color=discord.Color.blue(),
         )
         Embed.set_footer(self=embed,text='Nihongo.net')
-        Embed.add_field(self=embed,name='Kanji',value='- Random Kanji from N5,N4,N3,N2 and N1 collectivly',inline=True),
+        Embed.add_field(self=embed,name='kanji',value='- Random Kanji from N5,N4,N3,N2 and N1 collectivly',inline=True),
         Embed.add_field(self=embed,name='meme',value='- Random Meme',inline=True),
         await message.channel.send(embed=embed)
 
@@ -65,11 +68,9 @@ async def on_message(message):
         await message.channel.send(embed=embed)
 
 @client.event
-async def New_Member(member):#New Member code (Untested)
-    new_member = member.name
-    random_msg = {
-        "Nihongo.net へ ようこそ":"",
-        "Welcome To Nihongo.net":"",
-    }
-    await client.get_channel(902144083001212938).send(new_member + random.choice(random_msg))
+async def on_member_join(member):
+    channel = client.get_channel(902144083001212938)
+    embed=discord.Embed(title=f"Welcome {member.name}", description=f"Thanks for joining {member.guild.name}!")#F-Strings!
+    embed.set_thumbnail(url=member.avatar_url)#Set the embed's thumbnail to the member's avatar image!
+    await channel.send(embed=embed)
 client.run(token)
