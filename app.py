2f59f6c11a0ba1f2f8ea492d27cdb6909eb8eb76
Tallen123 <tallenthompson@icloud.com>
2021-10-30 23:19:47 +0100

    Fix

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ class Kanji:
         else:
             return render_template('KanjiN5.html',Color="#663399")
 
-    @app.route('/SearchPage/<string:kanji>')
+    @app.route('/SearchPage/<string:Kanji>')
     def SearchPage(kanji):
         if 'BackgroundColor' in session:
             return render_template('SearchPage.html',Color=session['BackgroundColor'],Kanji=kanji)
