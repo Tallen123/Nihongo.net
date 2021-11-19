4e12fd3b1afbb0038a69e4ecb230ed7d8ec7ef7d
Tallen <tallenthompson@icloud.com>
2021-11-19 23:15:51 +0000

    fixed some things with nav bar

--- nihongonet/nihongo/src/Components/Navbar.js
+++ nihongonet/nihongo/src/Components/Navbar.js
@@ -4,10 +4,18 @@ import $ from 'jquery'
 function Navbar() {
     $(document).ready(function(){
         $(document).mousemove(function(){
-             if($("#Alphabetbtn:hover").length != 0){
+             if($("#Alphabetbtn:hover").length !== 0){
+
+                var Alphabetbtn = $("#Alphabetbtn:hover")
+                var AlphabetMenu = document.getElementById("AlphabetMenu").style
+
+                AlphabetMenu.left = Alphabetbtn.position().left - 5 + "px"
                 $('#AlphabetMenu').show()
             }
-            if($("body:hover").length == 0){
+            if($("body:hover").length === 0){
+                $('#AlphabetMenu').hide()
+            }
+            if($("#homebtn:hover").length === 1){
                 $('#AlphabetMenu').hide()
             }
         })
@@ -15,13 +23,13 @@ function Navbar() {
 
     return (
     <div>
-        <div >
+        <div>
             <div className='inline m-3'>
-                <button className='btn'>Home</button>
+                <button id='homebtn' className='btn'>Home</button>
             </div>
             <div className='inline m-3'>
                 <button id='Alphabetbtn' className='btn' >Alphabet</button>
-                <div hidden id='AlphabetMenu'>
+                <div hidden id='AlphabetMenu' className='AlphabetMenu'>
                     <ul>
                         <li><button className='btn'>Hiragana</button></li>
                         <li><button className='btn'>Katakana</button></li>
