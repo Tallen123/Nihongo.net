e252928731a7d1c45b253f0b42e4c0cf2d87031b
Tallen <tallenthompson@icloud.com>
2021-11-21 16:59:35 +0000

    update

--- nihongonet/app.py
+++ nihongonet/app.py
@@ -6,3 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def HomePage():
     return render_template('HomePage.html')
+
+    
+@app.route('/beginnerOverview')
+def BeginnerOverview():
+    return render_template('BeginnerOverview.html')
+
+@app.route('/Beginner1Lesson1')
+def Beginner1Lesson1():
+    return render_template('/Beginner1/Beginner1Lesson1.html')#Beginner1Lesson1
+
+@app.route('/Hiragana')
+def Hiragana():
+    return render_template('/Beginner1/Alphabet/Hiragana.html')#Beginner1Lesson1
