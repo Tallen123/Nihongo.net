290fbda1123c8ea4559ed6d08a2613c4ea46f29b
Tallen <tallenthompson@icloud.com>
2021-11-26 14:43:46 +0000

    Index Update

--- nihongonet/app.py
+++ nihongonet/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
@@ -19,3 +18,12 @@ def Beginner1Lesson1():
 @app.route('/Hiragana')
 def Hiragana():
     return render_template('/Beginner1/Alphabet/Hiragana.html')#Beginner1Lesson1
+
+@app.route('/Index')
+def Index():
+    pages = {#add each page after they have been made
+        'Beginner1Lesson1':'/Beginner1Lesson1',
+        'Hiragana':'/Hiragana'
+    }
+    PageAmount= 2#add 1 per page added
+    return render_template('/Index.html', pages=pages,PageAmount=PageAmount)
